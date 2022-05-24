8d5cabe01326e702e24523df3c8264049792dacd
Chibikuro777 <56582396+Chibikuro777@users.noreply.github.com>
2022-05-24 18:10:38 +0900

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,38 @@
-'use strict';
-let gulp = require('gulp');
-let sass = require('sass');
-let cleanCSS = require('gulp-clean-css'); // minify
-let sourcemaps = require('gulp-sourcemaps'); // dev toolからscssの何行目か確認
-let sassGlob = require('gulp-sass-glob'); // パーシャルの一括読み込み
-let rename = require('gulp-rename'); // rename
-let autoprefixer = require('gulp-autoprefixer'); // ベンダープレフィックス
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css'); // minify
+const sourcemaps = require('gulp-sourcemaps'); // dev toolからscssの何行目か確認
+const sassGlob = require('gulp-sass-glob'); // パーシャルの一括読み込み
+const rename = require('gulp-rename'); // rename
+const autoprefixer = require('gulp-autoprefixer'); // ベンダープレフィックス
+
+//参照元パス
+const srcPath = {
+  css: 'src/scss/**/**.scss',
+}
+
+//出力先パス
+const destPath = {
+  css: 'css/',
+}
+
+// プラグインの処理をまとめる
+const cssSass = () => {
+  return src(srcPath.css) //コンパイル元
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(dest('assets/css'))     //コンパイル先
     .pipe(sourcemaps.init())
     .pipe(sassGlob())
     .pipe(sass({ outputStyle: 'compact' }))
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./assets/css'));
-});
-gulp.task('minify', function () {
-  return gulp.src("./assets/css/*.css")
+    .pipe(gulp.dest('./assets/css'))
     .pipe(cleanCSS())
     .pipe(rename({
       suffix: '.min',
     }))
-    .pipe(gulp.dest('./assets/css'));
-});
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', gulp.task(['sass']));
-});
+    .pipe(gulp.dest('./assets/css'))
+}
+
+exports.default = series(cssSass);
